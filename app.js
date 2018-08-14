b14b702a8102f080739d11c4c0029bc7d89f0d4a
Christopher Cooper <cjjcooper@gmail.com>
2018-08-14 16:16:30 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+
+// init app
+const app = express();
+
+// load view engine
+app.set('public', path.join(__dirname, 'public'));
+app.set('view engine', 'pug');
+app.use(express.static('views'));
+
+// home route
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+// start server
+app.listen(3000, function(){
+  console.log('Server started');
+});
