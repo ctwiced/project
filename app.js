09f83de692cd70d893a099e68e2a976c68eea1a6
Christopher Cooper <cjjcooper@gmail.com>
2018-09-11 12:15:47 -0600

    Got my changes back.

--- app.js
+++ app.js
@@ -1,22 +1,83 @@
 const express = require('express');
 const path = require('path');
-const Swiper = require('swiper');
+const multer = require('multer');
+const pug = require('pug');
+const port = 3000;
+const router = express.Router();
+const mongoose = require('mongoose');
+// const User = require('../lib/User')
 
-
-// init app
+// init app -----------------------------------------------------------------
 const app = express();
 
-// load view engine
+// load view engine -----------------------------------------------------------------
 app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'pug');
 app.use(express.static('views'));
 
-// home route
+// home route -----------------------------------------------------------------
 app.get('/', function(req, res){
   res.render('index');
 });
 
-// start server
-app.listen(3000, function(){
+// start server -----------------------------------------------------------------
+app.listen(port, function(){
   console.log('Server started');
 });
+
+// Server status -----------------------------------------------------------------
+router.get('/healthcheck', function(req, resp){
+  var responseString = 'OK';
+  res.send(responseString);
+})
+
+// User schema -----------------------------------------------------------------
+var userSchema = new mongoose.Schema({
+  username: {type: String, unique: true},
+  password: {type: String},
+  firstname: String,
+  lastname: String
+});
+
+var User = mongoose.model('myuser', userSchema)
+module.exports = User;
+
+// User authentication -----------------------------------------------------------------
+router.post('/login', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findeOne({username: username, password: password}, function(err, user) {
+    if(err) {
+      console.log(err);
+      return res.status(500).send();
+    }
+    if(!user) {
+      return res.status(404).send();
+    }
+    
+    return res.status(200).send();
+  })
+})
+
+// User Registration -----------------------------------------------------------------
+router.post('/register', function(req, res) {
+  var username = re.body.username;
+  var password = req.body.password;
+  var firstname = req.body.firstname;
+  var lastname = req.body.lastname;
+
+  var newuser = new User();
+  newuser.username = username;
+  newuser.password = password;
+  newuser.firstname = firstname;
+  newuser.lastname = lastname;
+  newuser.save(function(err, savedUser) {
+    if(err) {
+      console.log(err);
+      return res.status(500).send();
+    }
+  })
+})
+
+module.exports = router;
--- views/scripts.js
+++ views/scripts.js
@@ -1,26 +1,25 @@
-// Sign up modal script
+// -----------------------------------------------------------------------------------
+// Sign up modal script 
 
-var signUpModal = document.querySelector(".sign-up-modal");
-var signUpTrigger = document.querySelector(".sign-up-trigger");
-var signUpCloseButton = document.querySelector(".sign-up-close-button");
+// var signUpModal = document.querySelector(".sign-up-modal");
+// var signUpTrigger = document.querySelector(".sign-up-trigger");
+// var signUpCloseButton = document.querySelector(".sign-up-close-button");
 
-function toggleSignUpModal() {
-    signUpModal.classList.toggle("sign-up-show-modal");
-}
+// function toggleSignUpModal() {
+//     signUpModal.classList.toggle("sign-up-show-modal");
+// }
 
-function windowOnClickSignUp(event) {
-    if (event.target === signUpModal) {
-        toggleSignUpModal();
-    }
-}
+// function windowOnClickSignUp(event) {
+//     if (event.target === signUpModal) {
+//         toggleSignUpModal();
+//     }
+// }
 
-signUpTrigger.addEventListener("click", toggleSignUpModal);
-signUpCloseButton.addEventListener("click", toggleSignUpModal);
-window.addEventListener("click", windowOnClickSignUp);
+// signUpTrigger.addEventListener("click", toggleSignUpModal);
+// signUpCloseButton.addEventListener("click", toggleSignUpModal);
+// window.addEventListener("click", windowOnClickSignUp);
 
-
-
-
+// -----------------------------------------------------------------------------------
 // Sign in modal script
 
 var signInModal = document.querySelector(".sign-in-modal");
@@ -42,9 +41,7 @@ signInTrigger.addEventListener("click", toggleSignInModal);
 signInCloseButton.addEventListener("click", toggleSignInModal);
 window.addEventListener("click", windowOnClickSignIn);
 
-
-
-
+// -----------------------------------------------------------------------------------
 // Post event modal script
 
 var postModal = document.querySelector(".post-modal");
@@ -64,3 +61,56 @@ function windowOnClickPost(event) {
 postTrigger.addEventListener("click", togglePostModal);
 postCloseButton.addEventListener("click", togglePostModal);
 window.addEventListener("click", windowOnClickPost);
+
+// -----------------------------------------------------------------------------------
+// Item event modal script
+
+var itemModal = document.querySelector(".item-modal");
+var itemTrigger = document.querySelector(".item-trigger");
+var itemCloseButton = document.querySelector(".item-close-button");
+
+function toggleItemModal() {
+    itemModal.classList.toggle("item-show-modal");
+}
+
+function windowOnClickItem(event) {
+    if (event.target === itemModal) {
+        toggleItemModal();
+    }
+}
+
+itemTrigger.addEventListener("click", toggleItemModal);
+itemCloseButton.addEventListener("click", toggleItemModal);
+window.addEventListener("click", windowOnClickItem);
+
+// -----------------------------------------------------------------------------------
+// Dark and light mode
+
+function toggleDarkLight() {
+  var body = document.getElementById("body");
+  var currentClass = body.className;
+  body.className = currentClass == "dark-mode" ? "light-mode" : "dark-mode";
+}
+
+// -----------------------------------------------------------------------------------
+// Sticky Filter
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+} else {
+    navbar.classList.remove("sticky");
+}
+}
+
+// -----------------------------------------------------------------------------------
